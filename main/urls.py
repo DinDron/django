f2f01693e763852483a7a89b6cc0906003754e4c
DinDron <robertyaparov@mail.ru>
2023-01-15 22:16:29 +0500

    Интегрирована система добавление картинок через админ панель

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from djangoProject import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,7 @@ urlpatterns = [
     path('skills', views.skills, name='skills'),
     path('rv', views.rv, name='rv'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
